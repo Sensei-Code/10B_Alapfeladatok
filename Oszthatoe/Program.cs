1428e584ea20d7a5d90ccb9526f4122a89cec76a
Tóth Lajos <lajos.toth@windowslive.com>
2019-12-09 14:52:21 +0100

    Osztható-e feladat

---
+++ Oszthatoe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oszthatoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Kérek egy pozitív egész számot: ");
+            int szam = int.Parse(Console.ReadLine());
+
+            if (szam > 0)
+            {
+                if (szam % 7 == 0) Console.Write("A megadott szám osztható héttel!");
+                else Console.Write("A megadott szám NEM OSZTHATÓ héttel!");
+            }
+            else Console.Write("Hiba!! A megadott szám negatív!");
+
+            Console.ReadLine();
+        }
+    }
+}
