56b44a6d4b4de67920b889a6ae634e2ad65ce2e3
Tóth Lajos <lajos.toth@windowslive.com>
2020-01-08 11:13:41 +0100

    Ciklusok00

---
+++ CiklusWhile00/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiklusWhile00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int szam = 0;
+            int i = 6;
+
+            while (i < 5)
+            {
+                szam = rnd.Next(1, 100);
+                Console.Write("{0} ", szam);
+                i++;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
