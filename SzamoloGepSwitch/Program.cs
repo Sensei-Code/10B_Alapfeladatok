48d4a87e042279a1c811ca8a715d26c8cb973cc3
Tóth Lajos <lajos.toth@windowslive.com>
2020-01-29 09:24:19 +0100

    Ciklus és új számok bekérése

--- SzamoloGepSwitch/Program.cs
+++ SzamoloGepSwitch/Program.cs
@@ -13,6 +13,8 @@ namespace SzamoloGepSwitch
             //Készíts egy programot ami bekér két egész számot
             //és egy menürendszer segítségével kiválaszthatod, 
             //összeadod, kivonod, összeszorzod vagy elosztod őket.
+            //Lehessen új számokat is bekérni, valamint legyen egy menüpont
+            //a kilépésre.
 
             Console.Write("Kérem az A számot: ");
             double a = double.Parse(Console.ReadLine());
@@ -20,36 +22,58 @@ namespace SzamoloGepSwitch
             Console.Write("Kérem a B számot: ");
             double b = double.Parse(Console.ReadLine());
 
-            Console.Clear();
+            int menu = 0;
 
-            Console.WriteLine("A = {0}\t  B = {1}", a, b);
-            Console.WriteLine("\n1. A két szám összege");
-            Console.WriteLine("2. A két szám különbsége");
-            Console.WriteLine("3. A két szám szorzata");
-            Console.WriteLine("4. A két szám hányadosa");
-            Console.Write("\nVálassz egy menüpontot: ");
+            do {
 
-            int menu = int.Parse(Console.ReadLine());
+                Console.Clear();
 
-            switch (menu)
-            {
-                case 1:
-                    Console.Write("\nA két szám összege: {0}", a + b);
-                    break;
-                case 2:
-                    Console.Write("\nA két szám különbsége: {0}", a - b);
-                    break;
-                case 3:
-                    Console.Write("\nA két szám szorzata: {0}", a * b);
-                    break;
-                case 4:                    
-                    Console.Write("\nA két szám hányadosa: {0}", Math.Round(a/b,2));
-                    break;
-                default:
-                    Console.Write("Rossz menüpontot választottál!");
-                    break;
+                Console.WriteLine("A = {0}\t  B = {1}", a, b);
+                Console.WriteLine("\n1. A két szám összege");
+                Console.WriteLine("2. A két szám különbsége");
+                Console.WriteLine("3. A két szám szorzata");
+                Console.WriteLine("4. A két szám hányadosa");
+                Console.WriteLine("5. Új számok bekérése");
+                Console.WriteLine("6. Kilépés");
+                Console.Write("\nVálassz egy menüpontot: ");
 
-            }
+                menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        Console.WriteLine("\nA két szám összege: {0}", a + b);
+                        Console.WriteLine("Üss egy ENTER-t a folytatáshoz!");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("\nA két szám különbsége: {0}", a - b);
+                        Console.WriteLine("Üss egy ENTER-t a folytatáshoz!");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("\nA két szám szorzata: {0}", a * b);
+                        Console.WriteLine("Üss egy ENTER-t a folytatáshoz!");
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.WriteLine("\nA két szám hányadosa: {0}", Math.Round(a / b, 2));
+                        Console.WriteLine("Üss egy ENTER-t a folytatáshoz!");
+                        Console.ReadLine();
+                        break;
+                    case 5:
+
+
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        Console.Write("Rossz menüpontot választottál!");
+                        break;
+                }
+
+            } while (menu!=6);
+
             Console.ReadLine();
 
 
